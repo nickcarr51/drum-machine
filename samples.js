951bfefa59fa14fb9024114b467677900c897751
nickcarr51 <nick.carr84@gmail.com>
2020-06-21 20:23:50 -0400

    added s3

--- samples.js
+++ samples.js
@@ -1,19 +1,29 @@
-import kick from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/Decap_Sampler_Kick.mp3';
-import snare from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/decap_snare.mp3';
-import clap from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/clap.mp3'
-import rim from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/rim.mp3'
-import closedHat from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/closed_hat.mp3';
-import openCloseHat from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/openCloseHat.mp3';
-import openHat from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/openHat.mp3';
-import cowBell from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/808cowBell.mp3';
-import keys1 from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/keys1.mp3';
-import keys2 from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/keys2.mp3';
-import bass1 from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/bass1.mp3';
-import bass2 from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/bass2.mp3';
-import vox1 from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/vox1.mp3';
-import shaker from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/shaker.mp3';
-import crash from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/crash.mp3';
-import chimes from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/chimes.mp3';
+const aws = require('aws-sdk');
+
+
+let s3 = new aws.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    // accessKeyId: process.env.S3_KEY,
+    // secretAccessKey: process.env.S3_SECRET
+  });
+
+import kick from 's3://drummachinev1/Decap_Sampler_Kick.mp3';
+import snare from 's3://drummachinev1/decap_snare.mp3';
+import clap from 's3://drummachinev1/clap.mp3'
+import rim from 's3://drummachinev1/rim.mp3'
+import closedHat from 's3://drummachinev1/closed_hat.mp3';
+import openCloseHat from 's3://drummachinev1/openCloseHat.mp3';
+import openHat from 's3://drummachinev1/openHat.mp3';
+import cowBell from 's3://drummachinev1/808cowBell.mp3';
+import keys1 from 's3://drummachinev1/keys1.mp3';
+import keys2 from 's3://drummachinev1/keys2.mp3';
+import bass1 from 's3://drummachinev1/bass1.mp3';
+import bass2 from 's3://drummachinev1/bass2.mp3';
+import vox1 from 's3://drummachinev1/vox1.mp3';
+import shaker from 's3://drummachinev1/shaker.mp3';
+import crash from 's3://drummachinev1/crash.mp3';
+import chimes from 's3://drummachinev1/chimes.mp3';
 const kickSample = new Audio(kick);
 const snareSample = new Audio(snare);
 const clapSample = new Audio(clap);
@@ -49,3 +59,23 @@ export {
     crashSample,
     chimesSample
 }
+
+
+
+
+// import kick from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/Decap_Sampler_Kick.mp3';
+// import snare from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/decap_snare.mp3';
+// import clap from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/clap.mp3'
+// import rim from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/rim.mp3'
+// import closedHat from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/closed_hat.mp3';
+// import openCloseHat from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/openCloseHat.mp3';
+// import openHat from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/openHat.mp3';
+// import cowBell from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/808cowBell.mp3';
+// import keys1 from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/keys1.mp3';
+// import keys2 from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/keys2.mp3';
+// import bass1 from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/bass1.mp3';
+// import bass2 from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/bass2.mp3';
+// import vox1 from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/vox1.mp3';
+// import shaker from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/shaker.mp3';
+// import crash from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/crash.mp3';
+// import chimes from '/Users/Nickcarr/fullstack/practice/drum-machine/dist/chimes.mp3';
